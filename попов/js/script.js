f29422b3059279dbad32e39d5d48830aa4a6cabb
KirillWorks <podluzhnyy.kirill@yandex.ru>
2017-03-23 17:49:09 +0400

    23/03/17

---
+++ попов/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){ 
+  $('.head-slider').slick();
+  
+  $('.menu-slider').slick({
+       infinite: true,
+       slidesToShow: 3,
+       slidesToScroll: 1
+  });    
+});
