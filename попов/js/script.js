aa97dadace85e076eedc12196cab79b83a2c5511
KirillWorks <podluzhnyy.kirill@yandex.ru>
2017-03-24 18:25:40 +0400

    24/03/17

--- попов/js/script.js
+++ попов/js/script.js
@@ -1,9 +1,58 @@
 $(document).ready(function(){ 
-  $('.head-slider').slick();
+  $('.head-slider').slick({
+      centerMode: true,
+  centerPadding: '300px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
   
   $('.menu-slider').slick({
        infinite: true,
        slidesToShow: 3,
-       slidesToScroll: 1
-  });    
+       slidesToScroll: 1,
+       prevArrow:'<div class="PrevArrow"></div>',
+       nextArrow:'<div class="NextArrow"></div>',
+      responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
+    
+        
+  
 });
